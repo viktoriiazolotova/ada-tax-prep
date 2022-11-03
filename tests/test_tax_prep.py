148cfcb584f9dfb8e2b6530c16f9622fab9d929d
Viktoriia Zolotova <viktoriiazolotova@gmail.com>
2022-11-02 22:44:26 -0700

    adds sales tax deduction

--- tests/test_tax_prep.py
+++ tests/test_tax_prep.py
@@ -129,7 +129,16 @@ def few_valid_deductions():
         "mortgage": 5000,
         "child": 5000
     }    
-
+@pytest.fixture
+def new_valid_deductions():
+    return {
+        "charity": 5000,
+        "mortgage": 5000,
+        "child": 5000,
+        "tuition": 5000,
+        "healthcare": 5000,
+        "sales tax": 5000
+    }
 def test_deducted_income_cannot_fall_below_zero():
     income = 10000
 
@@ -157,6 +166,13 @@ def test_ignores_invalid_itemized_deductions(some_invalid_deductions):
     deducted_income = calculate_deducted_income_2020(income, some_invalid_deductions)
 
     assert deducted_income == 35000
+
+def test_applies_new_itemized_deductions(new_valid_deductions):
+    income = 50000
+
+    deducted_income = calculate_deducted_income_2020(income, new_valid_deductions)
+
+    assert deducted_income == 20000
 
 def test_calculate_adjusted_income_tax_burden(all_valid_deductions):
     income = 50000
